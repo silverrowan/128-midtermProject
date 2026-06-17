bacbea0045326de7f8fc2db6e363658192ecca49
Mariah Malczewska <mariahmalcz@gmail.com>
2026-06-17 11:45:26 -0700

    assign admin and mod rolls to users

--- script.js
+++ script.js
@@ -40,18 +40,25 @@ profileImage */
 // }
 
 //enter information into user objects
+//available roles are: admin, mod, and user, with user having the lowest priviledges
 for (let i = 0; i < baseUsers.length; i++) {
-    let fName = "fName" + i;
-    let lName = "lName" + i;
-    let username = `${fName}_${lName}`;
+    let username = `user${i}`;
 
-    baseUsers[i].firstName = fName;
-    baseUsers[i].lastName = lName;
+    baseUsers[i].userRole = "user";
+    baseUsers[i].firstName = "fName" + i;
+    baseUsers[i].lastName = "lName" + i;
     baseUsers[i].username = username;
     baseUsers[i].email = `${username}@email.com`;
-    // baseUsers[i].userRole = "user" + i;
     baseUsers[i].profileImage = `images/${username}`;
+    // username = baseUsers[i]; this didn't allow me to use username as objName
 }
 
-console.log(baseUsers);
+//Change 1 user's role to "admin"
+baseUsers[0].userRole = "admin";
+// baseUsers.user0.userRole = "admin";
 
+//Change 2 user's role to "mod" (moderator)
+baseUsers[1].userRole = "mod";
+baseUsers[2].userRole = "mod";
+
+console.log(baseUsers);
