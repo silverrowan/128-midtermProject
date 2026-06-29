bbb22cc52ca5c87bbfe68909135e1eef3ec1be23
Mariah Malczewska <mariahmalcz@gmail.com>
2026-06-29 10:24:30 -0700

    add file of generated uids

---
+++ baseUsers.js
@@ -0,0 +1,2 @@
+const baseUsers = [{"uid":"03LHN"},{"uid":"0JDHD"},{"uid":"65EO3"},{"uid":"8D8WN"},{"uid":"AC1A7"},{"uid":"DMEFU"},{"uid":"DOCMG"},{"uid":"K0Y9P"},{"uid":"Q2W6F"},{"uid":"QHH5H"},{"uid":"QSCSJ"},{"uid":"RQPSD"},{"uid":"RYBIX"},{"uid":"VIF0S"},{"uid":"YCQPM"}];
+Object.freeze(baseUsers);
